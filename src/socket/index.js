91bf2bc4f27b928476846e5af07c067e8f675014
Ygor Telmo <ygor.rodrigues@dafiti.com.br>
2020-09-10 19:08:07 -0300

    fix socket io for chat

--- src/socket/index.js
+++ src/socket/index.js
@@ -3,27 +3,14 @@ module.exports = function(server){
   var io = require('socket.io').listen(server);
 
   io.on('connection', socket => {
-    console.log('Entrou')
-    chatId = socket.handshake.query.chatId
+    chatId = socket.handshake.headers.chatid
     socket.join(chatId)
       socket.on('disconnect', () => {
         socket.leave(chatId)
       })
       socket.on('send_message', msg => {
-        created_at = msg.created_at,
-        chatId = msg.chatId,
-        userId = msg.userId,
-        message = msg.message
-        io.in(msg.chatId).emit('receive_message', {
-          'data': [{
-            'created_at': created_at,
-            'chatId': chatId,
-            'user': {
-              'id': userId
-            },
-            'message': message
-          }]
-        })
+        msgJson = JSON.parse(msg)
+        io.in(msgJson.chatId).emit('receive_message', msg)
       });
   });
 
